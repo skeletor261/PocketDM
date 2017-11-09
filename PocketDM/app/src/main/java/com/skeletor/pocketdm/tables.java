19e06fa8178fcafcc4ebe5f07dfde3534900f665
Andrew <stra.tbone@gmail.com>
2017-11-08 22:42:27 -0600

    Created tables.java File

---
+++ PocketDM/app/src/main/java/com/skeletor/pocketdm/tables.java
@@ -0,0 +1,19 @@
+package com.skeletor.pocketdm;
+
+/**
+ * Created by Andrew on 11/8/2017.
+ * This file is for storing the tables translated from C to Java
+ */
+
+public class tables {
+    int roll;
+    String outcome;
+
+
+}
+
+class critTable extends tables {
+    String weapon;
+
+    
+}
