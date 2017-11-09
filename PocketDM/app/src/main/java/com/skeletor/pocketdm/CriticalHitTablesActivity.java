2aa7c9c5c83d6c2c10dc762711a904560b4e1022
Andrew <stra.tbone@gmail.com>
2017-11-08 19:29:45 -0600

    Designed Preliminary Main Menu
    Main menu now has a simple design and critButton leads to CriticalHitTablesActivity

---
+++ PocketDM/app/src/main/java/com/skeletor/pocketdm/CriticalHitTablesActivity.java
@@ -0,0 +1,13 @@
+package com.skeletor.pocketdm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CriticalHitTablesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.critical_hit_tables);
+    }
+}
--- PocketDM/app/src/main/java/com/skeletor/pocketdm/MainActivity.java
+++ PocketDM/app/src/main/java/com/skeletor/pocketdm/MainActivity.java
@@ -1,13 +1,22 @@
 package com.skeletor.pocketdm;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main_menu);
+    }
+
+    public void toCritTables(View view) {
+        Intent critIntent = new Intent(this, CriticalHitTablesActivity.class);
+        startActivity(critIntent);
     }
 }
