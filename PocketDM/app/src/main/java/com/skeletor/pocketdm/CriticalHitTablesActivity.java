590db1ef4ec2826820ec2fa62a5dcacd0368c6ff
Andrew <stra.tbone@gmail.com>
2017-11-10 20:08:26 -0600

    Read input from rollInput

--- PocketDM/app/src/main/java/com/skeletor/pocketdm/CriticalHitTablesActivity.java
+++ PocketDM/app/src/main/java/com/skeletor/pocketdm/CriticalHitTablesActivity.java
@@ -2,12 +2,20 @@ package com.skeletor.pocketdm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class CriticalHitTablesActivity extends AppCompatActivity {
+
+    final EditText rollInput = (EditText) findViewById(R.id.rollInput);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.critical_hit_tables);
+    }
+
+    void onButtonClick(View weaponButton) {
+        int rollValue = Integer.parseInt(rollInput.getText().toString());
     }
 }
