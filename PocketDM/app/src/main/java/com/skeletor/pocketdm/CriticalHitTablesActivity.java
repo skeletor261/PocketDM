be7417a5aa490744ff597005e0b6f037272f3df3
Andrew <stra.tbone@gmail.com>
2017-11-21 15:09:30 -0600

    Fully functioning Crit table interaction
    Minor bug: resultText aligns with the right side of the screen instead of center when the text is longer than one line.

--- PocketDM/app/src/main/java/com/skeletor/pocketdm/CriticalHitTablesActivity.java
+++ PocketDM/app/src/main/java/com/skeletor/pocketdm/CriticalHitTablesActivity.java
@@ -12,28 +12,37 @@ import com.skeletor.pocketdm.tables;
 
 public class CriticalHitTablesActivity extends AppCompatActivity {
 
-    // Sets a class variable reference to the EditText input View object
-    final EditText rollInput = (EditText) findViewById(R.id.rollInput);
+    // Class Variables
+    //
+    // View Objects
+    private EditText rollInput;
+    private TextView resultText;
+    //
+    // Values
+    private int rollValue;
+    //
+    // Creates an empty HashMap for importing crit tables
+    private Map<Integer, String> weaponTable = new HashMap<Integer, String>() {};
 
-    // Sets a class variable reference to the TextView output View object
-    final TextView resultText = (TextView) findViewById(R.id.resultText);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.critical_hit_tables);
+
+        // Set View objects to class instances
+        rollInput = (EditText) findViewById(R.id.rollInput);
+        resultText = (TextView) findViewById(R.id.resultText);
     }
 
     // Determines which weapon button is pressed
     void onButtonClick(View weaponButton) {
 
         // Receive the value input for the percentile roll
-         int rollValue = Integer.parseInt(rollInput.getText().toString());
+        rollValue = Integer.parseInt(rollInput.getText().toString());
 
         // Check for rollValue > 100 or < 1 and return error message if true
-
-        // Creates an empty HashMap for importing crit tables
-        Map<Integer, String> weaponTable = new HashMap<Integer, String>() {};
+        //
 
         // Determines which weapon button is pressed and imports the corresponding table
         switch(weaponButton.getId()) {
