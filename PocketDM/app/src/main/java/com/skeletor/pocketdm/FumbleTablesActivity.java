370d44b39dc6e31787c0e7c7ee12a04bc4b19cd9
Unknown <stra.tbone@gmail.com>
2018-01-16 18:59:09 -0600

    Removed Unnecessary Import Statements

--- PocketDM/app/src/main/java/com/skeletor/pocketdm/FumbleTablesActivity.java
+++ PocketDM/app/src/main/java/com/skeletor/pocketdm/FumbleTablesActivity.java
@@ -3,10 +3,8 @@ package com.skeletor.pocketdm;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-
 import java.util.HashMap;
 import java.util.Map;
 
